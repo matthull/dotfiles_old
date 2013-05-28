663bb10bbf37d93d423afe4180ebb919d2e68815
Matt Hull <matt.hull@lacunasolutionstn.com>
2013-05-27 21:50:59 -0500

    added os-specific conditional

--- system/ubuntu/path.zsh
+++ system/ubuntu/path.zsh
@@ -1,1 +1,3 @@
-path=(/usr/lib/lightdm/lightdm /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin $path)
+if [[ $OSFAMILY == Linux ]]; then
+  path=(/usr/lib/lightdm/lightdm /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin $path)
+fi
