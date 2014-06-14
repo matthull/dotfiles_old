6b674865b4245de3f727b800a6ae225cbf8b74f8
Matt Hull <matt.hull@lacunasolutionstn.com>
2014-06-14 08:05:20 -0700

    updates

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,5 @@
 alias zshrc='$EDITOR $HOME/.zshrc && . $HOME/.zshrc'
 alias reload_dotfiles='. $HOME/.bootstrap && . $HOME/.zshrc'
 alias rld='reload_dotfiles'
+autoload -U zmv
+alias mmv='noglob zmv -W'
