5a6ad4e803d72e6e1c23129446983f289d5f45b5
Matt Hull <matt@matthull.io>
2015-01-05 10:14:41 -0800

    alias to wipe template files from cache

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,3 +1,4 @@
 alias websrv='ruby -run -ehttpd . -p8000'
 alias prm='bundle exec pry-remote'
 alias ttr='touch tmp/restart.txt'
+alias ct='(ag -alL "\\\$scope" tmp/cache/assets/development/sprockets | xargs ag -al templateCache.put | xargs rm) && ttr'
