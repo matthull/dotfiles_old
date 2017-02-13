ea976fc62fb03ddb8d02ba33adc0bcef0c1f9bed
Matthew Hull <matt@matthull.com>
2017-02-12 20:22:08 -0800

    more lists

--- t/aliases.zsh
+++ t/aliases.zsh
@@ -1,1 +1,4 @@
 alias t='python ~/bin/t/t.py --task-dir ~/Dropbox/tasks --list tasks'
+alias f='python ~/bin/t/t.py --task-dir ~/Dropbox/tasks --list focus'
+alias m='python ~/bin/t/t.py --task-dir ~/Dropbox/tasks --list misc'
+alias q='python ~/bin/t/t.py --task-dir ~/Dropbox/tasks --list questions'
